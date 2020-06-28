27846a6e6f842bb888e5a55966c2fa780ff33d48
Cody Roberts <red@pop-os.localdomain>
2020-06-27 20:52:26 -0500

    Functional walking with animations

--- src/main.ts
+++ src/main.ts
@@ -8,6 +8,7 @@ const sceneConfig: Phaser.Types.Scenes.SettingsConfig = {
   
   export class GameScene extends Phaser.Scene {
     private hero: Phaser.GameObjects.Sprite & { body: Phaser.Physics.Arcade.Body };
+    private isIdle = true;
    
     constructor() {
       super(sceneConfig);
@@ -16,43 +17,92 @@ const sceneConfig: Phaser.Types.Scenes.SettingsConfig = {
     }
 
     public preload() {
-      this.load.spritesheet('hero', 'assets/chara_hero.png', { frameWidth: 16, frameHeight: 16 })
+      this.load.spritesheet('hero', 'assets/chara_hero.png', { frameWidth: 48, frameHeight: 48 })
     }
    
     public create() {
-      this.hero = this.add.sprite(16, 16, 'hero', ) as any;
+      this.hero = this.add.sprite(48, 48, 'hero', ) as any;
       this.physics.add.existing(this.hero);
+      this.hero.setScale(2, 2)
+
 
       this.anims.create({
         key: 'idle',
-        frames: this.anims.generateFrameNumbers('hero', { start: 16, end: 48 }),
-        frameRate: 16,
+        frames: this.anims.generateFrameNumbers('hero', { start: 0, end: 2 }),
+        frameRate: 5,
         repeat: -1
     });
+
+      this.anims.create({
+        key: 'walk_down',
+        frames: this.anims.generateFrameNumbers('hero', {start: 8, end: 11}),
+        frameRate: 5,
+        repeat: -1
+      })
+
+      this.anims.create({
+        key: 'walk_up',
+        frames: this.anims.generateFrameNumbers('hero', {start: 16, end: 19}),
+        frameRate: 5,
+        repeat: -1
+      })
+
+      this.anims.create({
+        key: 'walk_sideways',
+        frames: this.anims.generateFrameNumbers('hero', {start: 12, end: 15}),
+        frameRate: 5,
+        repeat: -1
+      })
     }
    
     public update() {
       const controls = this.input.keyboard.createCursorKeys();
 
+      // TODO: Cut controls out into function. CLean up function with fewer if statements.
+      if(!controls.up.isDown && !controls.down.isDown && !controls.left.isDown && !controls.right.isDown){
+        this.hero.anims.play('idle', true);
+      }
+
       if(controls.up.isDown){
-        this.hero.body.setVelocityY(-500);
+        this.hero.body.setVelocityY(-50);
+        this.hero.anims.play('walk_up', true);
       }
       else if(controls.down.isDown){
-        this.hero.body.setVelocityY(500);
+        this.hero.body.setVelocityY(50);
+        this.hero.anims.play('walk_down', true);
       }
       else {
         this.hero.body.setVelocityY(0);
       }
 
       if(controls.left.isDown){
-        this.hero.body.setVelocityX(-500);
+        this.hero.body.setVelocityX(-50);
+        if(controls.down.isDown) {
+          this.hero.anims.play('walk_down', true);
+        }
+        else if(controls.up.isDown) {
+          this.hero.anims.play('walk_up', true)
+        } 
+        else {
+          this.hero.flipX = true;
+          this.hero.anims.play('walk_sideways', true);
+        }
       }
       else if(controls.right.isDown){
-        this.hero.body.setVelocityX(500);
+        this.hero.body.setVelocityX(50);
+        if(controls.down.isDown) {
+          this.hero.anims.play('walk_down', true);
+        }
+        else if(controls.up.isDown) {
+          this.hero.anims.play('walk_up', true)
+        } 
+        else {
+          this.hero.flipX = false;
+          this.hero.anims.play('walk_sideways', true);
+        }
       }
       else {
         this.hero.body.setVelocityX(0);
-        this.hero.anims.play('idle', true)
       }
     }
   }
